c187c9d413f99022260e06a8a058dcab2a52c6cd
panzerwaschbar <127913988+panzerwaschbar@users.noreply.github.com>
2024-01-27 15:05:38 -0500

    Stock Data

--- stockPlot.py
+++ stockPlot.py
@@ -7,4 +7,14 @@ import pandas as pd
 API_KEY = 
 symbol = 'AMZN'
 
-def fetch_stock_data(api_key, sym)
+def fetch_stock_data(api_key, symbol, interval='1d', output_size='compact'):
+    """
+    Fetch historical stock data from Alpha Vantage.
+    :param api_key: Alpha Vantage API api_key
+    :param symbol: Stock symbol
+    :param interval: Data interval(default is '1d' for daily)
+    """
+    ts = TimeSeries(key=api_key, output_format='pandas')
+    data, meta_data = ts.get_daily(symbol=symbol, outputsize=output_size)
+    return data
+
