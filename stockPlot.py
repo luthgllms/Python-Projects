90a5b83c280f03a21a221b93d1ea87640ac7531c
panzerwaschbar <127913988+panzerwaschbar@users.noreply.github.com>
2024-01-27 18:33:50 -0500

    Comments

--- stockPlot.py
+++ stockPlot.py
@@ -4,8 +4,8 @@ import matplotlib.pyplot as plt
 from alpha_vantage.timeseries import TimeSeries
 import pandas as pd
 
-API_KEY = 'TE7TY30VEWVGWQ64'
-symbol = 'AMZN'
+API_KEY = 'TE7TY30VEWVGWQ64' # Personal Alpha Vantage API Key
+symbol = 'AMZN' # Stock symbol
 
 def fetch_stock_data(api_key, symbol, interval='1d', output_size='compact'):
     """
@@ -17,6 +17,7 @@ def fetch_stock_data(api_key, symbol, interval='1d', output_size='compact'):
     return: Pandas DataFrame with stock data
     """
     ts = TimeSeries(key=api_key, output_format='pandas')
+    # Get Daily Stock Data
     data, meta_data = ts.get_daily(symbol=symbol, outputsize=output_size)
     return data
 
@@ -25,8 +26,11 @@ def plot_stock_data(stock_data):
     Plot stock data using Matplotlib.
     :param stock_data: Pandas DataFrame with stock data
     """
+    # Plot closing prices over time
     plt.figure(figsize=(12,6))
     plt.plot(stock_data['4. close'], label='Closing Price', color='blue')
+
+    # Labels, title, legend and grid
     plt.title(f'Stock Price Over Time - {symbol}')
     plt.xlabel('Date')
     plt.ylabel('Closing Price (USD)')
@@ -35,10 +39,15 @@ def plot_stock_data(stock_data):
     plt.show()
 
 def main():
+    # Fetch stock data using API key and symbol
     stock_data = fetch_stock_data(API_KEY, symbol)
+
+    #Plot the fetched stock data
     plot_stock_data(stock_data)
 
 if __name__ == "__main__":  
+
+    #Execute the main function if the scrpt is run directly
     main()
 
 
